eb1fbc8c0071d41bee6160e47dc723bfbee78c8a
ernestine-grossmann <e.grossmann@stud.uni-heidelberg.de>
2024-04-19 10:35:00 +0200

    Metis to cosmo with reduction info

--- app/metis_to_cosmo.cpp
+++ app/metis_to_cosmo.cpp
@@ -38,6 +38,48 @@
 #include "solution_check.h"
 #include "graph_operations.h"
 
+bool reduction_file_exists(const std::string & filename) {
+    std::ifstream file(filename);
+    return file.good();
+}
+void parse_reduction_data(const std::string & line, std::vector<bool> & reduction_data) {
+    std::istringstream iss(line);
+    std::string token;
+    for (size_t i = 0; i < reduction_data.size(); i++) {
+        std::getline(iss, token, ' ');
+        reduction_data[i] = std::stoi(token);
+    }
+}
+int write_weights_and_reductions_to_csv(graph_access & G, const std::string & filename) {
+    // if reduction file with filename exists, add the content, otherwise all 0
+    int num_of_reductions = 12;
+    std::vector<std::vector<bool>> reduction_data(num_of_reductions, std::vector<bool>(G.number_of_nodes(), false));
+    for (size_t i = 0; i < num_of_reductions; i++) {
+        if (reduction_file_exists("training_data/reduction_data/" + filename + "_reduction" + std::to_string(i) + ".txt")) {
+            std::ifstream reduction_file("training_data/reduction_data/" +filename + "_reduction" + std::to_string(i) + ".txt");
+            std::string line;
+            std::getline(reduction_file, line);
+            parse_reduction_data(line, reduction_data[i]);
+        }
+    }
+
+    std::ofstream f(filename.c_str());
+    f << "id;node_weight";
+    for (size_t i = 0; i < 12; i++) {
+        f << ";reduction" << i;
+    }
+
+    forall_nodes(G, node) {
+        f << node << ";" << G.getNodeWeight(node);
+        for (size_t i = 0; i < 12; i++) {
+            f << ";" << reduction_data[i][node];
+        }
+        f << std::endl;
+    } endfor
+
+    f.close();
+    return 0;
+}
 
 int main(int argn, char **argv) {
 
@@ -47,8 +89,7 @@ int main(int argn, char **argv) {
     if (ret_code) return 0;
 
     if (config.output_filename.empty()) {
-        std::cerr << "Output filename is empty." << std::endl;
-        return 0;
+        config.output_filename = config.graph_filename.substr(0, config.graph_filename.find_last_of('.'));
     }
     // read and set the graph
     std::string graph_filepath = config.graph_filename;
@@ -66,5 +107,6 @@ int main(int argn, char **argv) {
 
     go.writeGraphWeighted_to_csv(G, config.output_filename + ".csv");
     go.writeWeights_to_csv(G, config.output_filename + "_weight.csv");
+    write_weights_and_reductions_to_csv(G, config.output_filename + "_weights_and_reduction.csv");
     return 0;
 }
