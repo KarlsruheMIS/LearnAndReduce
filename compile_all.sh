c5e1c469244c4e36d87ceb915645615d7859f64a
ernestine-grossmann <e.grossmann@stud.uni-heidelberg.de>
2024-08-18 16:00:55 +0200

    add graphchecker

--- compile_all.sh
+++ compile_all.sh
@@ -26,7 +26,12 @@ cd build
 if [[ "$buildtype" == "Profile" ]]; then
   cmake ../ -DCMAKE_CXX_FLAGS=-pg -DCMAKE_EXE_LINKER_FLAGS=-pg -DCMAKE_SHARED_LINKER_FLAGS=-pg
 else
-    cmake ../ -DCMAKE_C_COMPILER=$(which gcc) -DCMAKE_CXX_COMPILER=$(which g++) -DCMAKE_BUILD_TYPE=${buildtype}
+    if [[ "$buildtype" == "Release" ]]; then
+        # cmake ../ -DCMAKE_C_COMPILER=$(which gcc) -DCMAKE_CXX_COMPILER=$(which g++) -DCMAKE_BUILD_TYPE=${buildtype} 
+        cmake ../ -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++
+    else
+        cmake ../ -DCMAKE_C_COMPILER=$(which gcc) -DCMAKE_CXX_COMPILER=$(which g++) -DCMAKE_BUILD_TYPE=${buildtype} -DREDUCTION_INFO=ON
+    fi
 fi
 make -j $NCORES
 cd ..
@@ -36,5 +41,6 @@ cp -f ./build/solver/branch_reduce/branch_reduce_convergence    deploy/branch_re
 cp -f ./build/solver/branch_reduce/kernelization                deploy/kernelization
 cp -f ./build/solver/branch_reduce/generate_training_data       deploy/generate_training_data
 cp -f ./build/metis_to_cosmo                                    deploy/metis_to_cosmo
+cp -f ./build/graphchecker                                      deploy/graphchecker
 
 
