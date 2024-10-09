adeda74bc9976fd6f6f16829ba9d36297c080273
KennethLangedal <kenneth.langedal@hotmail.com>
2024-10-09 15:44:20 +0200

    Added GNN features

--- training_data/training_new.py
+++ training_data/training_new.py
@@ -2,6 +2,7 @@ import csv
 import torch
 import random
 import sys
+import math
 
 import torch.nn.functional as F
 
@@ -11,7 +12,7 @@ dataset = []
 path = 'csv/'
 
 reduction = int(sys.argv[1])
-features = 4
+features = 8
 
 train = val = test = 0
 train_p = val_p = test_p = 0
@@ -36,25 +37,53 @@ with open('instances.txt', newline='') as instancefile:
             fields = list(reader)
 
         edge_index = torch.tensor(edge_list, dtype=torch.long)
-        x = torch.tensor([[row[1]] for row in fields], dtype=torch.float)
 
-        degree = [0 for _ in range(len(fields))]
+        weight = [row[1] for row in fields]
+        degree = [0.0 for _ in range(len(fields))]
         for u, v in edge_list:
-            degree[int(u)] += 1
+            degree[int(u)] += 1.0
 
-        neighborhood_degree = [0 for _ in range(len(fields))]
-        neighborhood_weight = [0 for _ in range(len(fields))]
+        neighborhood_degree = [0.0 for _ in range(len(fields))]
+        neighborhood_weight = [0.0 for _ in range(len(fields))]
+        
+        neighborhood_min_weight = [math.inf if degree[i] > 0 else 0.0 for i in range(len(fields))]
+        neighborhood_max_weight = [0.0 for _ in range(len(fields))]
+        neighborhood_min_degree = [math.inf if degree[i] > 0 else 0.0 for i in range(len(fields))]
+        neighborhood_max_degree = [0.0 for _ in range(len(fields))]
 
+        max_degree = 0.0
+        max_weight = 0.0
+        
         for u, v in edge_list:
             neighborhood_degree[int(u)] += degree[int(v)]
-            neighborhood_weight[int(u)] += x[int(v)][0].item()
+            neighborhood_weight[int(u)] += weight[int(v)]
 
+            neighborhood_min_weight[int(u)] = min(neighborhood_min_weight[int(u)], weight[int(v)])
+            neighborhood_max_weight[int(u)] = max(neighborhood_max_weight[int(u)], weight[int(v)])
+            neighborhood_min_degree[int(u)] = min(neighborhood_min_degree[int(u)], degree[int(v)])
+            neighborhood_max_degree[int(u)] = max(neighborhood_max_degree[int(u)], degree[int(v)])
+
+            max_degree = max(max_degree, degree[int(u)])
+            max_weight = max(max_weight, weight[int(u)])
+
+        for i in range(len(fields)):
+            if degree[i] == 0.0:
+                continue
+
+            neighborhood_weight[i] = weight[i] - neighborhood_weight[i]
+            neighborhood_degree[i] = (neighborhood_degree[i] / degree[i])
+
+        x0 = torch.tensor(weight, dtype=torch.float).unsqueeze(1)
         x1 = torch.tensor(neighborhood_weight, dtype=torch.float).unsqueeze(1)
-        x2 = torch.tensor(degree, dtype=torch.float).unsqueeze(1)
-        x3 = torch.tensor(neighborhood_degree, dtype=torch.float).unsqueeze(1)
+        x2 = torch.tensor(neighborhood_min_weight, dtype=torch.float).unsqueeze(1)
+        x3 = torch.tensor(neighborhood_max_weight, dtype=torch.float).unsqueeze(1)
+        x4 = torch.tensor(degree, dtype=torch.float).unsqueeze(1)
+        x5 = torch.tensor(neighborhood_degree, dtype=torch.float).unsqueeze(1)
+        x6 = torch.tensor(neighborhood_min_degree, dtype=torch.float).unsqueeze(1)
+        x7 = torch.tensor(neighborhood_max_degree, dtype=torch.float).unsqueeze(1)
 
-        x = torch.cat([x, x1, x2, x3], 1)
-        x = x / torch.max(x, 0)[0]
+        x = torch.cat([x0, x1, x2, x3, x4, x5, x6, x7], 1)
+
         
         y = torch.tensor([[row[reduction]] for row in fields], dtype=torch.float)
 
@@ -165,7 +194,7 @@ def train():
         optimizer.zero_grad()
 
         out = model(data.x, data.edge_index)
-        loss = F.binary_cross_entropy_with_logits(out[data.train_mask], data.y[data.train_mask], pos_weight=torch.tensor([20.0]))
+        loss = F.binary_cross_entropy(out[data.train_mask], data.y[data.train_mask]) # pos_weight=torch.tensor([10.0])
 
         loss.backward()
         optimizer.step()
@@ -215,16 +244,16 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('Training GCN')
 model = GCN(features, 16, 1).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-train_model(model, 2000)
+train_model(model, 600)
 
 print('Training SAGE')
 model = SAGE(features, 16, 1).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-train_model(model, 2000)
+train_model(model, 600)
 
 print('Training LR_CONV')
 model = LR_GCN(features, 16, 1).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-train_model(model, 2000)
+train_model(model, 600)
 
 store_model(model,  field_names[reduction] + '.lr_gcn')
