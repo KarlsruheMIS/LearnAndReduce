0d0d9f1ae6386d2b289b0186bda673969c211fb3
KennethLangedal <kenneth.langedal@hotmail.com>
2024-10-12 13:26:30 +0200

    Temporary implementation and test model

--- training_data/training_new.py
+++ training_data/training_new.py
@@ -115,7 +115,7 @@ from torch_geometric.nn import MessagePassing, GCNConv, SAGEConv, GENConv, GINEC
 
 class LRConv(MessagePassing):
     def __init__(self, in_channels, out_channels):
-        super().__init__(flow='target_to_source', aggr='max')
+        super().__init__(flow='target_to_source', aggr='mean')
         self.lin = torch.nn.Linear(2 * in_channels, out_channels)
         self.reset_parameters()
 
